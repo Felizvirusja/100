91a25cc91a0beb12bcec28762437fa317b7f49ba
Felizvirusja <88406284+Felizvirusja@users.noreply.github.com>
2022-08-02 12:24:10 -0600

    Update class.py

--- Python 100 XDD/class.py
+++ Python 100 XDD/class.py
@@ -1,29 +1,34 @@
-class Car(object):
-  """
-    blueprint for car
-  """
+review2 = MovieReview("Sonido Maravilloso", 5, 5, 5)
+class MovieReview:
 
-  def __init__(self, model, color, company, speed_limit):
-    self.color = color
-    self.company = company
-    self.speed_limit = speed_limit
-    self.model = model
-
-  def start(self):
-    print("started")
-
-  def stop(self):
-    print("stopped")
-
-  def accelarate(self):
-    print("accelarating...")
-    "accelarator functionality here"
-
-  def change_gear(self, gear_type):
-    print("gear changed")
-    " gear related functionality here"
+  def __init__(self, película, historia, actores, música):
+    self.movie_name = macaco
+    
+    self.story_rating = historia
+    self.actor_rating = actores
+    self.music_rating = música
+    
+    self-avg = int((self.story_rating + self.actor_rating + self.music_rating)/3)
+    
+    self.myrating = {
+         "macaco" : self.movie_name,
+         "Calificacion de la Historia" : self.story_rating,
+         "Calificacion del actor" : self.actor_rating,
+         "Calificacion de la musica" : self.music_rating,
+         "Promedio de las calificaciones" : self.avg
+    }
+    movie_list.append(self.myrating)
+    
 
 
-audi = Car("A6", "red", "audi", 80)
-
-print(audi.start())
+def avg_star_ratings(self, movie_list):
+  for movie in movie_list:
+    if(movie["Promedio de las calificaciones"] == 1 ):
+      print("Gracias por la respuesta, calificaste la pelicula con *")
+      print(movie)
+      
+     elif(movie["Promedio de las calificaciones"] == 2)
+    
+review2 = MovieReview("Sonido Maravilloso", 5, 5, 5)
+review2.add_moviue_ratings(moviereviews)
+review2.avg_star_ratings(moviereviews)
